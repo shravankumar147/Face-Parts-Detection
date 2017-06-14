88fa7fa2fd384f0da3dc6d9d63ad222d48274eb2
shravankumar147 <shravankumar147@gmail.com>
2017-06-14 17:57:45 +0530

    readme update

---
+++ result_writer.py
@@ -0,0 +1,10 @@
+import os
+
+flist = os.listdir("results/")
+
+print(flist)
+
+with open("result_write.txt","wb") as f:
+	for i in flist:
+		# ![Alt](results/face0_mouth.png)
+		f.write("![Alt](results/{})\n".format(i))
